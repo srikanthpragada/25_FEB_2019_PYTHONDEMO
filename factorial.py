b5425d2143d7b773de8e4a8c1a587bb63681df34
srikanthpragada <srikanthpragada@gmail.com>
2019-02-28 19:58:14 +0530

    28-FEB

---
+++ factorial.py
@@ -0,0 +1,9 @@
+# Factorial of the given number
+
+num = int(input("Enter a number :"))
+
+fact = 1
+for i in range(2, num + 1):
+    fact *= i
+
+print(f"Factorial of {num} is {fact}")
---
+++ for_demo.py
@@ -0,0 +1,4 @@
+# Print numbers from 1 to 10
+
+for n in range(10, 0, -2):
+    print(n, end=' ')
